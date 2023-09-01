c51194543b4009bcd60bac504a6c2ab400c27580
Mika Berglund <mika.berglund@outlook.com>
2023-09-01 13:17:42 +0300

    Added playground app for MAUI Blazor.

---
+++ PlaygroundMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlaygroundMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
