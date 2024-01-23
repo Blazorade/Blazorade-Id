1880ff8cd4c1be0da3c5691a1c91377d18eaa9fc
Mika Berglund <mika.berglund@outlook.com>
2024-01-23 12:01:54 +0200

    Added test app for doing MSAL authentication in a Blazor WASM application.

---
+++ BlazorWasmMsal/Program.cs
@@ -0,0 +1,19 @@
+using BlazorWasmMsal;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMicrosoftGraphClient("https://graph.microsoft.com/User.Read");
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+});
+
+await builder.Build().RunAsync();
