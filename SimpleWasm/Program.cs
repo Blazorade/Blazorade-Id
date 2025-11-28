48a39e629fedab64dc8363a6a32d7529d7080885
Mika Berglund <mika.berglund@integrata.fi>
2025-11-28 22:53:01 +0200

    Update Blazorade.Id integration and token management
    Updated the `Microsoft.AspNetCore.Components.Web` package to version 8.0.17. Introduced token management in `Home.razor` with `TokenService` injection and a new button to retrieve access tokens. Enhanced service configuration in `Program.cs` to include `Blazorade.Id` services and authority settings. Added `Blazorade.Id.Services` and `Blazorade.Id.Core.Services` namespaces to `_Imports.razor` for easier access across the app.

--- SimpleWasm/Program.cs
+++ SimpleWasm/Program.cs
@@ -6,6 +6,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazoradeCore()
+    .AddBlazoradeIdWasmApplication()
+        .AddAuthority((sp, options) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            config.GetRequiredSection("blazorade:id").Bind(options);
+        })
+    ;
 
 await builder.Build().RunAsync();
