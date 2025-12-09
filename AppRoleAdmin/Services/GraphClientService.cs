a1243eec53c55c7a3a40fa58f629b557cac061b8
Mika Berglund <mika.berglund@integrata.fi>
2025-12-09 22:40:56 +0200

    Add app selection and editing workflow to UI
    Introduced ApplicationSelectorDrawer and ApplicationEditor components for interactive Azure AD app selection and inspection. Home page now uses a FAB to open the selector. Application details, app roles, and assigned principals are displayed. Extended GraphClientService with methods for fetching applications, service principals, and app role assignments. Refactored Scopes class and improved ApplicationSelector UI.

--- AppRoleAdmin/Services/GraphClientService.cs
+++ AppRoleAdmin/Services/GraphClientService.cs
@@ -1,5 +1,5 @@
-﻿using AppRoleAdmin.Security;
-using Azure.Core;
+﻿using Azure.Core;
+using Blazorade.Id.Core.Configuration;
 using Blazorade.Id.Core.Services;
 using Microsoft.Graph;
 using Microsoft.Graph.Applications;
@@ -18,6 +18,24 @@ namespace AppRoleAdmin.Services
         private readonly ITokenService TokenService;
         private readonly IHttpClientFactory HttpFactory;
 
+
+        public async Task<Application?> GetApplicationAsync(string? id)
+        {
+            if(id?.Length > 0)
+            {
+                var graph = await this.GetGraphClientAsync(scope: Scopes.ApplicationReadAll);
+                if (null != graph)
+                {
+                    var app = await graph.Applications[id].GetAsync(config =>
+                    {
+                        config.QueryParameters.Select = ["id", "appId", "displayName", "appRoles"];
+                    });
+                    return app;
+                }
+            }
+
+            return null;
+        }
         public async IAsyncEnumerable<Application> GetApplicationsAsync(string? query)
         {
             var graph = await this.GetGraphClientAsync(scope: Scopes.ApplicationReadAll);
@@ -36,12 +54,68 @@ namespace AppRoleAdmin.Services
 
                 foreach(var app in appsResponse?.Value ?? [])
                 {
-                    yield return app;
+                    if(null != app)
+                    {
+                        yield return app;
+                    }
                 }
             }
 
             yield break;
         }
+
+        public async Task<ServicePrincipal?> GetServicePrincipalAsync(Application app)
+        {
+            var graph = await this.GetGraphClientAsync(scope: Scopes.ApplicationReadAll);
+            if(null != graph)
+            {
+                var response = await graph.ServicePrincipals.GetAsync(config =>
+                {
+                    config.QueryParameters.Filter = $"appId eq '{app.AppId}'";
+                });
+
+                return response?.Value?.FirstOrDefault();
+            }
+
+            return null;
+        }
+
+        public async IAsyncEnumerable<AppRoleAssignment> GetAppRoleAssignmentsAsync(ServicePrincipal principal, AppRole role)
+        {
+            var graph = await this.GetGraphClientAsync(scope: $"{Scopes.ApplicationReadAll} {Scopes.DirectoryReadAll}");
+            if(null != graph)
+            {
+                var response = await graph.ServicePrincipals[principal.Id].AppRoleAssignedTo.GetAsync(config =>
+                {
+                    
+                });
+                while(null != response)
+                {
+                    foreach (var assignment in response.Value ?? [])
+                    {
+                        if (null != assignment && assignment.AppRoleId == role.Id)
+                        {
+                            yield return assignment;
+                        }
+                    }
+
+                    if(response.OdataNextLink?.Length > 0)
+                    {
+                        response = await graph.ServicePrincipals[principal.Id]
+                            .AppRoleAssignments
+                            .WithUrl(response.OdataNextLink)
+                            .GetAsync();
+                    }
+                    else
+                    {
+                        response = null;
+                    }
+                }
+            }
+
+            yield break;
+        }
+
 
 
         private async Task<GraphServiceClient?> GetGraphClientAsync(string? scope)
@@ -58,6 +132,14 @@ namespace AppRoleAdmin.Services
             }
 
             return null;
+        }
+
+
+        public static class Scopes
+        {
+            public const string ApplicationReadAll = "Application.Read.All";
+            public const string ApplicationReadWriteAll = "Application.ReadWrite.All";
+            public const string DirectoryReadAll = "Directory.Read.All";
         }
     }
 }
