586468fd83c8895310df932c94d2bc816d0cdc79
Mika Berglund <mika.berglund@integrata.fi>
2025-12-22 16:19:45 +0200

    Add configurable service registration overloads to builder
    Added Action<IServiceProvider, TService> overloads for key service registration methods in BlazoradeIdBuilder, enabling post-construction configuration of DI service instances for greater flexibility.

--- Blazorade.Id.Core/Configuration/BlazoradeIdBuilder.cs
+++ Blazorade.Id.Core/Configuration/BlazoradeIdBuilder.cs
@@ -56,6 +56,21 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the given authentication state notifier to the service collection with the option to configure
+        /// the service after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddAuthenticationStateNotifier<TAuthenticationStateNotifier>(Action<IServiceProvider, TAuthenticationStateNotifier> config) where TAuthenticationStateNotifier: class, IAuthenticationStateNotifier
+        {
+            this.Services.AddScoped<IAuthenticationStateNotifier, TAuthenticationStateNotifier>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TAuthenticationStateNotifier>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the authentication state notifier used in the application.
         /// </summary>
         /// <param name="config">The delegate that creates the authentication state notifier to use in the application.</param>
@@ -81,6 +96,21 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the authorization code failure notifier specified in <typeparamref name="TAuthorizationCodeFailureNotifier"/> to the service collection,
+        /// and provides you with the option to set properties on the authorization code processor after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddAuthorizationCodeFailureNotifier<TAuthorizationCodeFailureNotifier>(Action<IServiceProvider, TAuthorizationCodeFailureNotifier> config) where TAuthorizationCodeFailureNotifier : class, IAuthorizationCodeFailureNotifier
+        {
+            this.Services.AddScoped<IAuthorizationCodeFailureNotifier, TAuthorizationCodeFailureNotifier>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TAuthorizationCodeFailureNotifier>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the authorization code failure notifier used in the application.
         /// </summary>
         public BlazoradeIdBuilder AddAuthorizationCodeFailureNotifier(Func<IServiceProvider, IAuthorizationCodeFailureNotifier> config)
@@ -96,6 +126,21 @@ namespace Blazorade.Id.Configuration
         public BlazoradeIdBuilder AddAuthorizationCodeProcessor<TAuthorizationCodeProcessor>() where TAuthorizationCodeProcessor : class, IAuthorizationCodeProcessor
         {
             this.Services.AddScoped<IAuthorizationCodeProcessor, TAuthorizationCodeProcessor>();
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the authorization code processor specified in <typeparamref name="TAuthorizationCodeProcessor"/> to the service collection,
+        /// and provides you with the option to set properties on the authorization code processor after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddAuthorizationCodeProcessor<TAuthorizationCodeProcessor>(Action<IServiceProvider, TAuthorizationCodeProcessor> config) where TAuthorizationCodeProcessor : class, IAuthorizationCodeProcessor
+        {
+            this.Services.AddScoped<IAuthorizationCodeProcessor, TAuthorizationCodeProcessor>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TAuthorizationCodeProcessor>(sp);
+                config(sp, svc);
+                return svc;
+            });
             return this;
         }
 
@@ -119,6 +164,21 @@ namespace Blazorade.Id.Configuration
         }
         
         /// <summary>
+        /// Adds the authorization code provider specified in <typeparamref name="TAuthorizationCodeProvider"/> to the service collection,
+        /// and provides you with the option to set properties on the authorization code provider after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddAuthorizationCodeProvider<TAuthorizationCodeProvider>(Action<IServiceProvider, TAuthorizationCodeProvider> config) where TAuthorizationCodeProvider : class, IAuthorizationCodeProvider
+        {
+            this.Services.AddScoped<IAuthorizationCodeProvider, TAuthorizationCodeProvider>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TAuthorizationCodeProvider>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the authorization code provider used in the application.
         /// </summary>
         public BlazoradeIdBuilder AddAuthorizationCodeProvider(Func<IServiceProvider, IAuthorizationCodeProvider> config)
@@ -134,6 +194,21 @@ namespace Blazorade.Id.Configuration
         public BlazoradeIdBuilder AddEndpointService<TEndpointService>() where TEndpointService : class, IEndpointService
         {
             this.Services.AddScoped<IEndpointService, TEndpointService>();
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the endpoint service specified in <typeparamref name="TEndpointService"/> to the service collection,
+        /// and provides you with the option to set properties on the endpoint service after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddEndpointService<TEndpointService>(Action<IServiceProvider, TEndpointService> config) where TEndpointService : class, IEndpointService
+        {
+            this.Services.AddScoped<IEndpointService, TEndpointService>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TEndpointService>(sp);
+                config(sp, svc);
+                return svc;
+            });
             return this;
         }
 
@@ -157,6 +232,20 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the property storage specified in <typeparamref name="TPropertyStore"/> to the service collection,
+        /// and provides you with the option to set properties on the property store after it has been created.
+        public BlazoradeIdBuilder AddPropertyStore<TPropertyStore>(Action<IServiceProvider, TPropertyStore> config) where TPropertyStore : class, IPropertyStore
+        {
+            this.Services.AddScoped<IPropertyStore, TPropertyStore>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TPropertyStore>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the property storage to use in the application.
         /// </summary>
         public BlazoradeIdBuilder AddPropertyStore(Func<IServiceProvider, IPropertyStore> config)
@@ -172,6 +261,21 @@ namespace Blazorade.Id.Configuration
         public BlazoradeIdBuilder AddRedirectUriProvider<TRedirectUriProvider>() where TRedirectUriProvider : class, IRedirectUriProvider
         {
             this.Services.AddScoped<IRedirectUriProvider, TRedirectUriProvider>();
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the redirect URI provider specified in <typeparamref name="TRedirectUriProvider"/> to the service collection,
+        /// and provides you with the option to set properties on the redirect URI provider after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddRedirectUriProvider<TRedirectUriProvider>(Action<IServiceProvider, TRedirectUriProvider> config) where TRedirectUriProvider : class, IRedirectUriProvider
+        {
+            this.Services.AddScoped<IRedirectUriProvider, TRedirectUriProvider>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TRedirectUriProvider>(sp);
+                config(sp, svc);
+                return svc;
+            });
             return this;
         }
 
@@ -199,6 +303,21 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the scope sorter specified in <typeparamref name="TScopeSorter"/> to the service collection,
+        /// and provides you with the option to set properties on the scope sorter after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddScopeSorter<TScopeSorter>(Action<IServiceProvider, TScopeSorter> config) where TScopeSorter : class, IScopeSorter
+        {
+            this.Services.AddScoped<IScopeSorter, TScopeSorter>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TScopeSorter>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the scope sorter used in the application.
         /// </summary>
         /// <remarks>
@@ -222,6 +341,21 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the sign-out service specified in <typeparamref name="TSignOutService"/> to the service collection,
+        /// and provides you with the option to set properties on the sign-out service after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddSignOutService<TSignOutService>(Action<IServiceProvider, TSignOutService> config) where TSignOutService : class, ISignOutService
+        {
+            this.Services.AddScoped<ISignOutService, TSignOutService>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TSignOutService>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the sign-out service to use in the application.
         /// </summary>
         public BlazoradeIdBuilder AddSignOutService(Func<IServiceProvider, ISignOutService> config)
@@ -241,6 +375,21 @@ namespace Blazorade.Id.Configuration
         }
 
         /// <summary>
+        /// Adds the token refresher specified in <typeparamref name="TTokenRefresher"/> to the service collection,
+        /// and provides you with the option to set properties on the token refresher after it has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddTokenRefresher<TTokenRefresher>(Action<IServiceProvider, TTokenRefresher> config) where TTokenRefresher : class, ITokenRefresher
+        {
+            this.Services.AddScoped<ITokenRefresher, TTokenRefresher>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TTokenRefresher>(sp);
+                config(sp, svc);
+                return svc;
+            });
+            return this;
+        }
+
+        /// <summary>
         /// Adds the token refresher to use in the application.
         /// </summary>
         public BlazoradeIdBuilder AddTokenRefresher(Func<IServiceProvider, ITokenRefresher> config)
@@ -256,6 +405,23 @@ namespace Blazorade.Id.Configuration
         public BlazoradeIdBuilder AddTokenStore<TTokenStore>() where TTokenStore : class, ITokenStore
         {
             this.Services.AddScoped<ITokenStore, TTokenStore>();
+            return this;
+        }
+
+        /// <summary>
+        /// Adds the token store specified in <typeparamref name="TTokenStore"/> to the service collection,
+        /// and provides you with the option to set properties on the token store after it
+        /// has been created.
+        /// </summary>
+        public BlazoradeIdBuilder AddTokenStore<TTokenStore>(Action<IServiceProvider, TTokenStore> config) where TTokenStore : class, ITokenStore
+        {
+            this.Services.AddScoped<ITokenStore, TTokenStore>(sp =>
+            {
+                var svc = ActivatorUtilities.CreateInstance<TTokenStore>(sp);
+                config(sp, svc);
+                return svc;
+            });
+
             return this;
         }
 
