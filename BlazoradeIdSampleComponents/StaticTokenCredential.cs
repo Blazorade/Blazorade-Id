778dee1782df9dd0eddb4ec9a32f97c9847945b0
Mika Berglund <mika.berglund@integrata.fi>
2025-12-28 09:18:46 +0200

    Add Microsoft Graph integration and user info sample page
    Added Azure.Core and Microsoft.Graph dependencies. Introduced StaticTokenCredential for bridging Blazorade.Id tokens with SDKs requiring TokenCredential. Updated navigation to include a new "Using Microsoft Graph" page, which demonstrates acquiring a token and fetching user profile data from Microsoft Graph using the GraphServiceClient.

---
+++ BlazoradeIdSampleComponents/StaticTokenCredential.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+
+namespace BlazoradeIdSampleComponents
+{
+    public class StaticTokenCredential : TokenCredential
+    {
+
+        public StaticTokenCredential(string token, DateTimeOffset expiresOn)
+        {
+            this.AccessToken = new AccessToken(token, expiresOn);
+        }
+
+        private readonly AccessToken AccessToken;
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            return ValueTask.FromResult(this.AccessToken);
+        }
+
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            return this.AccessToken;
+        }
+
+    }
+}
