3011ca883c7894516ae8eb9000ab165b999a9489
Mika Berglund <mika.berglund@integrata.fi>
2025-12-28 11:01:21 +0200

    Add Blazor Web namespace to components; cleanup usings
    Added Microsoft.AspNetCore.Components.Web to key Razor components for event handling. Refactored StaticTokenCredential.cs by removing redundant usings and moving Azure.Core inside the namespace. Updated _Imports.razor to remove global Web import and fixed formatting.

--- BlazoradeIdSampleComponents/StaticTokenCredential.cs
+++ BlazoradeIdSampleComponents/StaticTokenCredential.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Azure.Core;
-
+﻿
 namespace BlazoradeIdSampleComponents
 {
+    using Azure.Core;
+
     public class StaticTokenCredential : TokenCredential
     {
 
@@ -26,6 +22,5 @@ namespace BlazoradeIdSampleComponents
         {
             return this.AccessToken;
         }
-
     }
 }
